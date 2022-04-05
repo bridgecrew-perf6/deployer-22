6659b19a5596dfa40995d6722043da2238226ae7
deployer <deployer>
2022-04-06 03:28:50 +0800

    upgrade pancake contracts to 0.8.0

--- contracts/contracts/v2-periphery/libraries/UniswapV2Library.sol
+++ contracts/contracts/v2-periphery/libraries/UniswapV2Library.sol
@@ -19,12 +19,12 @@ library UniswapV2Library {
     // calculates the CREATE2 address for a pair without making any external calls
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
+        pair = address(uint160(uint256(keccak256(abi.encodePacked(
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
                 hex'f38e070c980c6f6b0654a71ae60445c65b9735195216455d454cf591af1b8e35' // init code hash
-            ))));
+            )))));
     }
 
     // fetches and sorts the reserves for a pair
