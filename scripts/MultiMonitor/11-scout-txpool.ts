10cb837b1cefbcf16e8d1241087e133d56241ca8
deployer <deployer>
2022-04-10 04:26:09 +0800

    feat: add check

--- scripts/MultiMonitor/11-scout-txpool.ts
+++ scripts/MultiMonitor/11-scout-txpool.ts
@@ -13,31 +13,6 @@ let deploymentsObj = require('./mainnet/bsc-56-deploy-MultiBevBot.json');
 let args = [];
 const fs = require('fs');
 
-/*
-{
-  hash: '0xba06c835ecaa090a8cfab529fb78611d0b420ec4eb96796c74a96dbe243adad3',
-  type: 0,
-  accessList: null,
-  blockHash: null,
-  blockNumber: null,
-  transactionIndex: null,
-  confirmations: 0,
-  from: '0x4e84E02b811D67C5D3Aa865b96642e1b5F820B50',
-  gasPrice: BigNumber { value: "5000000000" },
-  gasLimit: BigNumber { value: "321807" },
-  to: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
-  value: BigNumber { value: "1400000000000000000" },
-  nonce: 13379,
-  data: '0xb6f9de950000000000000000000000000000000000000000000003beb0f73e6400f5aeb500000000000000000000000000000000000000000000000000000000000000800000000000000000000000004e84e02b811d67c5d3aa865b96642e1b5f820b5000000000000000000000000000000000000000000000000000000000623d8c850000000000000000000000000000000000000000000000000000000000000003000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c000000000000000000000000e9e7cea3dedca5984780bafc599bd69add087d560000000000000000000000003e63e9c8f2297e3c027f8444b4591e2583d8780b',
-  r: '0xce85146077c93c6c2fb6e27bce25b518ae906418e5c89a7d85e2b5784f9e71ff',
-  s: '0x0e5e325aad81ca4ec5be1a70e34f49e935a18f70d201ed4cad731f69e23a5788',
-  v: 148,
-  creates: null,
-  chainId: 56,
-  wait: [Function (anonymous)]
-}
-* */
-
 let provider: any
 
 // -----------------------------------------------------------------------------
@@ -65,6 +40,10 @@ const equalAddress = (a: string, b: string) => {
 }
 
 const handlePendingTx = async (txParam: any ) => {
+    if (!txParam) {
+        return
+    }
+
     let tx: any
     if (txParam.from && txParam.to) {
         tx = txParam
@@ -149,3 +128,28 @@ main()
         console.error(error);
         process.exit(1);
     });
+
+/*
+{
+  hash: '0xba06c835ecaa090a8cfab529fb78611d0b420ec4eb96796c74a96dbe243adad3',
+  type: 0,
+  accessList: null,
+  blockHash: null,
+  blockNumber: null,
+  transactionIndex: null,
+  confirmations: 0,
+  from: '0x4e84E02b811D67C5D3Aa865b96642e1b5F820B50',
+  gasPrice: BigNumber { value: "5000000000" },
+  gasLimit: BigNumber { value: "321807" },
+  to: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+  value: BigNumber { value: "1400000000000000000" },
+  nonce: 13379,
+  data: '0xb6f9de950000000000000000000000000000000000000000000003beb0f73e6400f5aeb500000000000000000000000000000000000000000000000000000000000000800000000000000000000000004e84e02b811d67c5d3aa865b96642e1b5f820b5000000000000000000000000000000000000000000000000000000000623d8c850000000000000000000000000000000000000000000000000000000000000003000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c000000000000000000000000e9e7cea3dedca5984780bafc599bd69add087d560000000000000000000000003e63e9c8f2297e3c027f8444b4591e2583d8780b',
+  r: '0xce85146077c93c6c2fb6e27bce25b518ae906418e5c89a7d85e2b5784f9e71ff',
+  s: '0x0e5e325aad81ca4ec5be1a70e34f49e935a18f70d201ed4cad731f69e23a5788',
+  v: 148,
+  creates: null,
+  chainId: 56,
+  wait: [Function (anonymous)]
+}
+* */
